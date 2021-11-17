0f9aafc3ee4d7828772c86cf1ce5811a74475703
theofbraune <theo@rieffel-braune.de>
2021-11-17 11:15:10 +0100

    Chaning the
    functions

--- detector.py
+++ detector.py
@@ -9,7 +9,7 @@ def main(framerate = 20, scale=1.5):
     Input: The framerate in FPS and the scale of the video window
     Output: The video feed from the webcam
     """
-    frame_buffer = []
+    frame_buffer_object = []
     #here later add the feature to use a different webcam, not just the default one
     cap = cv2.VideoCapture(0)
 
@@ -26,23 +26,28 @@ def main(framerate = 20, scale=1.5):
         fr, forehead = face_detection.forehead_detection(frame, coordinates)
         #here manipulate the frame
         #cut out the face here
-        frame_buffer.append(forehead)
+
+        if forehead.shape[0]!=0:
+            frame_buffer_object.append(forehead)
         #how many frames do we want to pass?
-        if(len(frame_buffer)==20):
+        if(len(frame_buffer_object)==150):
             #remove the first frame from the buffer
-            frame_buffer.pop(0)
-        
+            frame_buffer_object.pop(0)
+        #here call t
         #pass the pictures to the function to compute the buffer
-        #if(counter<20):
+        #if(counter<500):
         #    cv2.imwrite("/home/theo/Documents/ecole_polytechnique/computer_vision/project/X-INF573-heart-rater/test_forhead/pic_%i.png"%counter, forehead) 
-        #rate_compute.detect_change(frame_buffer)
+        
+        #tuning of the parameters of the buffer size and the framerate to see when it is good.
+        if(len(frame_buffer_object)==150):
+            rate_compute.detect_change(frame_buffer_object)
         cv2.imshow('Input', fr)
 
         c = cv2.waitKey(int(1000//framerate))
-        counter+=1
+        #counter+=1
 
     cap.release()
     cv2.destroyAllWindows()
 
 
-main(framerate = 50, scale = 1.5)
+main(framerate = 10, scale = 1.5)
--- rate_compute.py
+++ rate_compute.py
@@ -3,29 +3,60 @@ import cv2
 import matplotlib.pyplot as plt
 
 #create the data to test the function
-"""
+
 buffer = []
-for j in range(20):
+for j in range(150):
     im = cv2.imread('/home/theo/Documents/ecole_polytechnique/computer_vision/project/X-INF573-heart-rater/test_forhead/pic_%i.png'%j)
     buffer.append(im)
-"""
+
 
 def detect_change(buffer_object):
-    """Input: buffer object, a sequence of frames
     """
+    Input: buffer object, a sequence of frames
+    Output: 
+    """
+    
     min_y = buffer_object[0].shape[0]
     min_x = buffer_object[0].shape[1]
     for j in range(len(buffer_object)):
-        if(buffer_object[j].shape[1]<min_x):
-            min_x = buffer_object[j].shape[1]
-        if(buffer_object[j].shape[0]<min_y):
-            min_y = buffer_object[j].shape[0]
+        #if the programm failed to generate a 
+        if(buffer_object[j].shape[0]==0):
+            buffer_object[j] = buffer_object[j-1]
+        else:
+            if(buffer_object[j].shape[1]<min_x):
+                min_x = buffer_object[j].shape[1]
+            if(buffer_object[j].shape[0]<min_y):
+                min_y = buffer_object[j].shape[0]
         
 
     for idx in range(len(buffer_object)):
         #cut the pictures in the right size
         buffer_object[idx] = buffer_object[idx][:min_y,:min_x,::]
     
+    #for idx in range(len(buffer_object)):
+    #    print(buffer_object[j].shape)
+    #calculate the maximum red green and blue value, plot it over the frame
+    
+    #maybe better change to numpy objects.. see whether this is a problem.
+    
+    #red_channel = buffer_object[:,:,:,0]
+    buffer_np = np.array(buffer_object)
+    red_channel = buffer_np[:,:,:,0]
+    green_channel = buffer_np[:,:,:,1]
+    blue_channel = buffer_np[:,:,:,2]
+    
+    #compute the mean per image to get an array that corresponds to the 
+    x_red = red_channel.mean(axis=(1,2))
+    x_green = green_channel.mean(axis=(1,2))
+    x_blue = blue_channel.mean(axis=(1,2))
+
+    #normalize the vector over the whole time
+    x_red =  x_red/np.linalg.norm(x_red)
+    x_green =  x_green/np.linalg.norm(x_green)
+    x_blue =  x_blue/np.linalg.norm(x_blue)
+    
+    
+    
     
 
-#detect_change(buffer)
+detect_change(buffer)
